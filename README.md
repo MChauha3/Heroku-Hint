85309dc7c592b269144bae5310d2a8c8961af8c2
monikachauhan4 <mona.munch@gmail.com>
2021-03-09 12:03:55 +0530

    heroku integration

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Heroku-Hint
+# Heroku Marketing Cloud API Demo
+
+ A simple POC of Salesforce Marketing Cloud API using Heroku
+
+
+For more information:
+
+- [@leafarhz](https://twitter.com/leafarhz)
+- [Rafael Hernandez](https://www.linkedin.com/in/leafarhz/)
+
---
+++ public/js/customActivity.js
@@ -0,0 +1,89 @@
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
+    'use strict';
+
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
+
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+
+    }
+
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
+        }
+
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+        console.log(inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+
+            });
+        });
+
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+
+
+    }
+
+    function onGetTokens(tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        console.log(tokens);
+        authTokens = tokens;
+    }
+
+    function onGetEndpoints(endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        console.log(endpoints);
+    }
+
+    function save() {
+
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+        payload.name = name;
+
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "emailAddress": "sunny.bansal@customactivity.com"
+        }];
+
+        payload['metaData'].isConfigured = true;
+
+        console.log(payload);
+
+        connection.trigger('updateActivity', payload);
+    }
+
+});
